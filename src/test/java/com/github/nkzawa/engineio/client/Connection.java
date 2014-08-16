d2aecdb155c64fe9d00ad0d26837f6c5f357a272
Naoyuki Kanezawa <naoyuki.kanezawa@gmail.com>
2014-08-16 20:24:56 +0900

    increase timeout

--- src/test/java/com/github/nkzawa/engineio/client/Connection.java
+++ src/test/java/com/github/nkzawa/engineio/client/Connection.java
@@ -10,7 +10,7 @@ import java.util.concurrent.*;
 
 public abstract class Connection {
 
-    final static int TIMEOUT = 3000;
+    final static int TIMEOUT = 10000;
     final static int PORT = 3000;
 
     private Process serverProcess;
